372419d02dd0891c5dc37dc605b9c487c129bb7c
Pjaerr <washingtonfaulconer@gmail.com>
2019-04-22 09:34:41 +0100

    Added files where only change is through prettier

--- src/utils/getPlaces.js
+++ src/utils/getPlaces.js
@@ -2,38 +2,32 @@ import getTransformedVenueData from './getTransformedVenueData';
 
 //TODO: Add checks in here for if no locations are found so can return an empty array instead of Foursquare array.
 
-const getPlaces = (query, limit, pos) =>
-{
-	return new Promise((resolve, reject) =>
-	{
-		let latLng = `${pos.coords.latitude},${pos.coords.longitude}`;
+const getPlaces = (query, limit, pos) => {
+  return new Promise((resolve, reject) => {
+    let latLng = `${pos.coords.latitude},${pos.coords.longitude}`;
 
-		let url = `/placedata/
+    let url = `/placedata/
 						latLng=${latLng}&
 						limit=${limit}&
 						query=${query}`;
 
-		fetch(url)
-			.then(response => response.json())
-			.then(data =>
-			{
-				getTransformedVenueData(data.response.groups[0].items)
-					.then(venues => 
-					{
-						resolve(venues);
-					})
-					.catch(err =>
-					{
-						reject(err);
-						console.error("Error getting transformed venue data \n Error: " + err);
-					});
-			})
-			.catch(err =>
-			{
-				reject(err);
-				console.error("Error whilst fetching place data \n Error: " + err);
-			});
-	});
-}
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        getTransformedVenueData(data.response.groups[0].items)
+          .then(venues => {
+            resolve(venues);
+          })
+          .catch(err => {
+            reject(err);
+            console.error('Error getting transformed venue data \n Error: ' + err);
+          });
+      })
+      .catch(err => {
+        reject(err);
+        console.error('Error whilst fetching place data \n Error: ' + err);
+      });
+  });
+};
 
 export default getPlaces;
