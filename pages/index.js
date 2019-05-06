4cabfced4f1ab94f5e58d77d0bd1484901439304
Pjaerr <washingtonfaulconer@gmail.com>
2019-05-06 17:22:51 +0100

    Added a bunch more categories. (thier images are local)

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,29 @@ const categories = [
     name: 'Attractions',
     subCategories: [
       { displayName: 'Museums', query: 'Museums' },
-      { displayName: 'Historic', query: 'Historic' }
+      { displayName: 'Theme Parks', query: 'Theme Parks' },
+      { displayName: 'Historic', query: 'Historic' },
+      { displayName: 'Art', query: 'Art' },
+      { displayName: 'Theatre', query: 'Theatre' },
+      { displayName: 'Zoos', query: 'Zoos' }
     ]
   },
   {
     name: 'Sights',
     subCategories: [
       { displayName: 'Landmarks', query: 'Landmarks' },
-      { displayName: 'Nature', query: 'Nature' }
+      { displayName: 'Lakes', query: 'Lakes' },
+      { displayName: 'Parks', query: 'Parks' },
+      { displayName: 'City Views', query: 'City Views' }
+    ]
+  },
+  {
+    name: 'Social',
+    subCategories: [
+      { displayName: 'Nightlife', query: 'Nightlife' },
+      { displayName: 'Pubs', query: 'Pubs' },
+      { displayName: 'Sports', query: 'Sports' },
+      { displayName: 'Community', query: 'Community' }
     ]
   },
   {
@@ -24,7 +39,11 @@ const categories = [
       { displayName: 'Chinese', query: 'Chinese Food' },
       { displayName: 'American', query: 'American Food' },
       { displayName: 'German', query: 'German Food' },
-      { displayName: 'Spanish', query: 'Spanish Food' }
+      { displayName: 'Spanish', query: 'Spanish Food' },
+      { displayName: 'Vietnamese', query: 'Vietnamese Food' },
+      { displayName: 'Thai', query: 'Thai Food' },
+      { displayName: 'Indian', query: 'Indian Food' },
+      { displayName: 'Japanese', query: 'Japanese Food' }
     ]
   }
 ];
