359c61c49aa508a242074a20c619df1e53f8c18b
Pjaerr <washingtonfaulconer@gmail.com>
2019-05-13 20:23:51 +0100

    Moved Category styles into their own file

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Link from 'next/link';
-
-import styled from 'styled-components';
-import GlobalStyles from '../../../GlobalStyles';
-
 import SubCategory from '../SubCategory/SubCategory';
 
-const MainCategoryContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-
-  h3 {
-    margin-bottom: -10px;
-    border-bottom: 0.25px solid ${GlobalStyles.lightTextColour};
-  }
-`;
-
-const SubCategoryContainer = styled.ul`
-  overflow-x: auto;
-  display: flex;
-  padding: 20px;
-  width: 90%;
-  white-space: nowrap;
-  margin-right: 10px;
-`;
+import * as Styled from './CategoryStyles';
 
 const Category = ({ name, subCategories }) => {
   return (
-    <MainCategoryContainer>
+    <Styled.MainCategoryContainer>
       <h3>{name}</h3>
 
-      <SubCategoryContainer>
+      <Styled.SubCategoryContainer>
         <SubCategory showImage={false} displayName="Everything" query={name} />
 
         {subCategories.map(subCategory => (
@@ -44,8 +20,8 @@ const Category = ({ name, subCategories }) => {
             query={subCategory.query}
           />
         ))}
-      </SubCategoryContainer>
-    </MainCategoryContainer>
+      </Styled.SubCategoryContainer>
+    </Styled.MainCategoryContainer>
   );
 };
 
