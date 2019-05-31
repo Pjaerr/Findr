74f3d355b1cb966fccf69e0d347fa4231d9bd240
Joshua Jackson <=>
2019-05-31 07:39:08 +0100

    Removed code that has been converted

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const express = require('express');
-
+//Controllers
 const placeData = require('./controllers/placeData.js');
 
+const express = require('express');
 const app = express();
 const port = 3001;
 
@@ -12,28 +12,6 @@ app.get('/', (req, res) => res.send('Invalid Route!'));
 app.get('/placedata/latLng=:latLng&limit=:limit&query=:query', placeData);
 
 app.listen(port, () => console.log(`Backend listening on port ${port}!`));
-
-// app
-//   .prepare()
-//   .then(() => {
-//     const server = express();
-
-//     server.get('/placedata/latLng=:latLng&limit=:limit&query=:query', (req, res) => {
-//       let limit = 'limit=' + req.params.limit + '&';
-//       let ll = 'll=' + req.params.latLng + '&';
-//       let query = 'query=' + req.params.query;
-
-//       let apiQuery =
-//         foursquareApiData.venueBaseUrl + client_id + client_secret + v + limit + ll + query;
-
-//       request(apiQuery, function(error, response, body) {
-//         if (error) console.log('error:', error);
-
-//         if (body) {
-//           res.send(body);
-//         }
-//       });
-//     });
 
 //     server.get('/placedata/photos/id=:locationId', (req, res) => {
 //       let locationId = req.params.locationId + '?';
@@ -49,17 +27,3 @@ app.listen(port, () => console.log(`Backend listening on port ${port}!`));
 //         }
 //       });
 //     });
-
-//     server.get('*', (req, res) => {
-//       return handle(req, res);
-//     });
-
-//     server.listen(3001, err => {
-//       if (err) throw err;
-//       console.log('> --Backend--\nReady on http://localhost:3001');
-//     });
-//   })
-//   .catch(ex => {
-//     console.error(ex.stack);
-//     process.exit(1);
-//   });
