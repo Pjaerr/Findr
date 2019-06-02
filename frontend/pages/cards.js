4e4d7b63de62616d9b096e8867f337895963eec2
Pjaerr <=>
2019-06-02 14:37:38 +0100

    Catch getPlaces error to avoid error message being shown to user

--- frontend/pages/cards.js
+++ frontend/pages/cards.js
@@ -32,13 +32,17 @@ const cards = ({ router }) => {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
         pos => {
-          getPlaces(category, 15, LONDON_LATLNG).then(places => {
-            if (places.length <= 0) {
-              setNoPlacesNearby(true);
-            } else {
-              setPointsOfInterestData(places);
-            }
-          });
+          getPlaces(category, 15, LONDON_LATLNG)
+            .then(places => {
+              if (places.length <= 0) {
+                setNoPlacesNearby(true);
+              } else {
+                setPointsOfInterestData(places);
+              }
+            })
+            .catch(err => {
+              console.error(err);
+            });
 
           // setPointsOfInterestData(testData);
         },
